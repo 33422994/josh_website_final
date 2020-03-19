47b9be340b52b9e7fe0d94da814cef8fcff2db79
33422994 <61783001+33422994@users.noreply.github.com>
2020-03-19 17:07:39 +0800

    Initial commit

---
+++ footer.php
@@ -0,0 +1,24 @@
+<footer class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 text-center footer-content">
+<p> <?php bloginfo('name'); ?></p>
+                <img src=" http://206.189.45.97/~mesh9/wp-content/themes/starter_for_josh_site/images/tumblr.png" alt=“tumblr” >
+                <img src=" http://206.189.45.97/~mesh9/wp-content/themes/starter_for_josh_site/images/rss.png" alt=“rss” >
+            </div>
+            <div class="col-md-6 text-center footer-content">
+                <p>Terms and Conditions</p>
+                <img src=" http://206.189.45.97/~mesh9/wp-content/themes/starter_for_josh_site/images/linkedin.png" alt=“linkedin” >
+                <img src=" http://206.189.45.97/~mesh9/wp-content/themes/starter_for_josh_site/images/fb.png" alt=“facebook” >
+            </div>
+
+        </div>
+    </div>
+</footer>
+
+<!—DELETE ALL JAVASCRIPT -->
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+<?php wp_footer(); ?>
